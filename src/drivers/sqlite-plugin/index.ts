f7c34f868312f0a17266d41d3399b191fd96a201
James Arthur <thruflo@gmail.com>
2022-10-10 11:39:55 +0200

    sqlite-plugin: add addStatement method.

--- src/drivers/sqlite-plugin/index.ts
+++ src/drivers/sqlite-plugin/index.ts
@@ -25,6 +25,11 @@ export interface SQLitePlugin {
 export interface SQLitePluginTransaction {
   readOnly: boolean
   success(...args: any[]): any
+
+  // Never promisified.
+  addStatement(sql: string, values?: BindParams, success?: AnyFunction, error?: AnyFunction): void
+
+  // May be promisified.
   executeSql(sql: string, values?: BindParams, success?: AnyFunction, error?: AnyFunction): VoidOrPromise
 }
 
